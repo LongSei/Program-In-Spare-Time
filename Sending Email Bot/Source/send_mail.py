ff79f682654d15d7d29215698deb05b82be86990
LongSei <74856548+LongSei@users.noreply.github.com>
2021-10-06 16:18:57 +0700

    Add files via upload

---
+++ Sending Email Bot/Source/send_mail.py
@@ -0,0 +1,33 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+username = os.getenv('gmail_username')
+password = os.getenv('gmail_password')
+print(username)
+print(password)
+
+def send_mail(text='Email Body', subject="", from_email="", to_emails=None, html=None):
+    assert isinstance(to_emails, list)
+    msg = MIMEMultipart('alternative')
+    msg['From'] = from_email
+    msg['To'] = ", ".join(to_emails)
+    msg['Subject'] = subject
+    txt_part = MIMEText(text, 'plain')
+    msg.attach(txt_part)
+    if html != None:
+        html_part = MIMEText(html, 'html')
+        msg.attach(html_part)
+    msg_str = msg.as_string()
+
+
+    # Login to the server
+    server = smtplib.SMTP(host='smtp.gmail.com', port=587)
+    server.ehlo()
+    server.starttls()
+    server.login(username, password)
+    server.sendmail(from_email, to_emails, msg_str)
+    server.quit()
